ea8c440807594b758bc7e778689bd1ecd8b4fc5f
shyamzzp <shyamsundersuthar009@gmail.com>
2020-04-14 16:46:40 +0530

    Updates

---
+++ precache-manifest.305fcc9887d27b053fe23637df6c4c8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b4b012911784cafe5fccdb2f34543a",
+    "url": "/attendance/index.html"
+  },
+  {
+    "revision": "5cda53fcbd1d2fbe8378",
+    "url": "/attendance/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "d69314e80eeb01c3c562",
+    "url": "/attendance/static/css/main.7b9ee055.chunk.css"
+  },
+  {
+    "revision": "5cda53fcbd1d2fbe8378",
+    "url": "/attendance/static/js/2.d594be38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/attendance/static/js/2.d594be38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69314e80eeb01c3c562",
+    "url": "/attendance/static/js/main.5e48ef56.chunk.js"
+  },
+  {
+    "revision": "9885e8651e27b8d05661",
+    "url": "/attendance/static/js/runtime-main.f7bb9b1d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/attendance/static/media/logo.ee7cd8ed.svg"
+  }
+]);
